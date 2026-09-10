722cadc9b84449188592975ed50653854378cbe3
Mgreat01 <ephraimmonga5@gmail.com>
2026-09-10 07:32:08 +0200

    Ajouter la page d'acces refuse

---
+++ frontend/src/app/shared/pages/access-denied/access-denied.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-access-denied',
+  imports: [RouterLink],
+  templateUrl: './access-denied.component.html',
+})
+export class AccessDeniedComponent {}
