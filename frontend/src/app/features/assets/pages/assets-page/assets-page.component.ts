c3ded2b7712b0fe2afbf766d1113c55d31d6bd00
Mgreat01 <ephraimmonga5@gmail.com>
2026-09-13 16:02:28 +0200

    Charger les membres dans l'inventaire

--- frontend/src/app/features/assets/pages/assets-page/assets-page.component.ts
+++ frontend/src/app/features/assets/pages/assets-page/assets-page.component.ts
@@ -1,11 +1,12 @@
 import { Component, computed, effect, inject, signal } from '@angular/core';
 
-import { ASSET_CATEGORY_LABELS, Asset, AssetCategory } from '../../../../core/models/governance.models';
+import { ASSET_CATEGORY_LABELS, Asset, AssetCategory, MemberOption } from '../../../../core/models/governance.models';
 import { ContextService } from '../../../../core/services/context.service';
 import { TopbarComponent } from '../../../../shared/components/topbar/topbar.component';
 import { DicBadgeComponent } from '../../../../shared/components/dic-badge/dic-badge.component';
 import { AssetFormModalComponent } from '../../components/asset-form-modal/asset-form-modal.component';
 import { AssetsService } from '../../services/assets.service';
+import { UsersService } from '../../../users/services/users.service';
 
 @Component({
   selector: 'app-assets-page',
@@ -15,6 +16,7 @@ import { AssetsService } from '../../services/assets.service';
 export class AssetsPageComponent {
   protected readonly assetsService = inject(AssetsService);
   protected readonly context = inject(ContextService);
+  protected readonly usersService = inject(UsersService);
   protected readonly search = signal('');
   protected readonly category = signal<AssetCategory | 'ALL'>('ALL');
   protected readonly modalOpen = signal(false);
@@ -23,6 +25,9 @@ export class AssetsPageComponent {
   protected readonly successMessage = signal('');
   protected readonly categoryLabels = ASSET_CATEGORY_LABELS;
   protected readonly categories = Object.entries(ASSET_CATEGORY_LABELS) as [AssetCategory, string][];
+  protected readonly members = computed<MemberOption[]>(() => this.usersService.users()
+    .filter((user) => user.is_active)
+    .map((user) => ({ id: user.id, name: user.name })));
 
   protected readonly filteredAssets = computed(() => {
     const query = this.search().trim().toLocaleLowerCase('fr');
@@ -45,7 +50,10 @@ export class AssetsPageComponent {
   constructor() {
     effect(() => {
       const scopeId = this.context.activeScopeId();
-      if (scopeId) this.assetsService.fetchAssets(scopeId);
+      if (scopeId) {
+        this.assetsService.fetchAssets(scopeId);
+        this.usersService.fetch();
+      }
     });
   }
 
