eb6c79719733e3fbdc6c145cc8f72b6dd4b7a4fa
Jovialngandu <jovialngandu2@gmail.com>
2026-09-16 23:32:27 +0100

    [UPDATE] Modification de la settig pour la prise en compte en prod

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -9,23 +9,27 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
+import dj_database_url
+
 
 from pathlib import Path
+from dotenv import load_dotenv
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Forcer le chargement du fichier .env à la racine
+env_path = BASE_DIR / '.env'
+load_dotenv(dotenv_path=env_path, override=True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a%g_gi&t6dpzeg*lo+ypeu5rm#z(xv-9!l$afjjhe!ed#30x-#'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-a%g_gi&t6dpzeg*lo+ypeu5rm#z(xv-9!l$afjjhe!ed#30x-#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 
 # Application definition
@@ -78,12 +82,31 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+
+# --- DATABASE CONFIGURATION ---
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            conn_health_checks=True,
+            # Activer SSL uniquement si l'URL contient sslmode=require (ex: Neon)
+            ssl_require='sslmode=require' in DATABASE_URL
+        )
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
+
 
 
 # Password validation
